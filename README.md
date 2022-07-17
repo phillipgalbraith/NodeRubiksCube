5577315561219d6c9fb7688159081e52de6a1422
Phil G <fillupgalbraith@gmail.com>
2022-07-17 15:30:09 -0700

    column rotation bug discovered

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # NodeRubiksCube
+This application contains functions for rotating a Rubik's Cube's rows and columns
+
+## Naming sides and moves
+
+The frontside is that which is facing the solver.
+
+The rightside is that which is on the solver's right-hand-side 
+
+Rotating a column up is to rotate that column such that the column's frontside squares move to the topside.
+
+Rotating a row left is to rotate that row such that the row's frontside squares move to the leftside
+
+## Square Layout of a side
+
+    upper row   blue square            blue square                blue square
+    middle row  blue square            blue square                blue square
+    lower row   blue square            blue square                blue square
+
+                left column            middle colummn             right Column
+
+ [  
+    upper-left, upper-middle, upper-right, 
+    middle-left, middle-middle, middle-right, 
+    lower-left, lower-middle, lower-right
+]
+
+## Side Layout of starter Cube
+
+                        top
+
+        front   left    back    right
+
+                        bottom
+
+[frontside, leftside, backside, rightside, topside, bottomside]
+
+## Logic of corresponding sides
+
+The Top of each horizontal side must be aligned with all other upper rows.
+... middle rows to middle rows, lower row to lower rows
+
+Rotating the left column up moves the causes:
+ the frontside upper-left square to be in a position we will call the top
+ Therefore the square that was once the frontside upper-left is in the topside upper-left position
+    "Topside-Frontside alignment"
+    
+    Therefore we use a reverse layout for the backside and bottomside
+    "bottomside-backside alignment"
+
+### Vertical Coordinate mirroring
+    crossing into an unaligned side through column rotation produces 180 degree rotated coordinates
+
+    column up :     topside upper-left -> backside lower-right
+                    bottomside upper-left -> frontside lower-right
--- rotate/columns.js
+++ rotate/columns.js
@@ -1,55 +1,59 @@
-export function rotateLeftRowUp(rCube) {
+import { counterClockwiseRotator, clockwiseRotator } from "../utils/constants.js";
+export function rotateLeftColumnUp(rCube) {
     const priorCube = [];
     rCube.forEach( e => priorCube.push([ ...e]))
 
-   
     //rotateVertically facing sides
     let i=0;
     while ( i<=6 ){
-        i+=3
-        const coresp5= [ ...priorCube[5][i]].reduce( (a,b) => a+b ) // corresponding square on bottom
+        const j = 8 - i // 180 degree rotated coordinates
+        console.log({j})
+        // *rotating from the bottom to the front is a v180 deg orientation
+        const coresp5= [ ...priorCube[5]][j] // corresponding square on bottom is 180 orientation
         rCube[0].splice(i,1,coresp5) // priorCube[5][i] // front will be bottom
         
-        const coresp2 = [ ...priorCube[2][i]].reduce( (a,b) => a+b ) // corresponding square on back
-        rCube[5].splice(i,1,coresp2) // = priorCube[2][i] // bottom will be back
+        const coresp2 = [ ...priorCube[2]][j] // corresponding square on backside
+        rCube[5].splice(j,1,coresp2) // = priorCube[2][i] // bottom will be replaced by back
 
-        const coresp4 = [ ...priorCube[4][i]].reduce( (a,b) => a+b ) // corresponding square on top
-        rCube[2].splice(i,1,coresp4) // = priorCube[3][i] // back will be top
+        const coresp4 = [ ...priorCube[4]][i] // corresponding square on top
+        rCube[2].splice(j,1,coresp4) // = priorCube[3][i] // backside bottom-right will be topside upper-left
         
-        const coresp0= [ ...priorCube[0][i]].reduce( (a,b) => a+b ) // corresponding square on top
-        rCube[4].splice(i,1,coresp0) //= priorCube[0][i] // top will be front
+        const coresp0= [ ...priorCube[0]][i] // corresponding square on front
+        rCube[4].splice(i,1,coresp0) //= priorCube[2][i] // top will be replaced by front
+
+        i += 3
     }
 
-    //rotateHorizontally facing side on left
-        //counterClockwiseRotation topRow[2, 5, 8] middleRow[1,4,7] bottomRow[0,3,6]
-
     rCube[1].forEach( (e, i) => rCube[1][i] = priorCube[1][counterClockwiseRotator[i]])
+
 }
 
-export function rotateLeftRowDown(rCube) {
+export function rotateLeftColumnDown(rCube) {
     const priorCube = [];
     rCube.forEach( e => priorCube.push([ ...e]))
 
-   
+    //rotateVertically facing sides
     //rotateVertically facing sides
     let i=0;
     while ( i<=6 ){
-        i+=3
-        const coresp5= [ ...priorCube[5][i]].reduce( (a,b) => a+b ) // corresponding square on bottom
-        rCube[0].splice(i,1,coresp5) // priorCube[5][i] // front will be bottom
+        const j = 8 - i // 180 degree rotated coordinates
+        console.log({j})
+        // *rotating from the bottom to the front is a v180 deg orientation
+        const coresp5= [ ...priorCube[5]][j] // corresponding square on bottom is 180 orientation
+        rCube[2].splice(j,1,coresp5) // back will be bottom
         
-        const coresp2 = [ ...priorCube[2][i]].reduce( (a,b) => a+b ) // corresponding square on back
-        rCube[5].splice(i,1,coresp2) // = priorCube[2][i] // bottom will be back
+        const coresp2 = [ ...priorCube[2]][j] // corresponding square on backside
+        rCube[4].splice(i,1,coresp2) // top will be replaced by back
 
-        const coresp4 = [ ...priorCube[4][i]].reduce( (a,b) => a+b ) // corresponding square on top
-        rCube[2].splice(i,1,coresp4) // = priorCube[3][i] // back will be top
-        
-        const coresp0= [ ...priorCube[0][i]].reduce( (a,b) => a+b ) // corresponding square on top
-        rCube[4].splice(i,1,coresp0) //= priorCube[0][i] // top will be front
+        const coresp4 = [ ...priorCube[4]][i] // corresponding square on top
+        rCube[2].splice(i,1,coresp4) // front will be topside         
+       
+        const coresp0= [ ...priorCube[0]][i] // corresponding square on front
+        rCube[5].splice(j,1,coresp0) //= priorCube[2][i] // bottom will be replaced by front
+
+        i += 3
     }
 
-    //rotateHorizontally facing side on left
-        //counterClockwiseRotation topRow[2, 5, 8] middleRow[1,4,7] bottomRow[0,3,6]
+    rCube[1].forEach( (e, i) => rCube[1][i] = priorCube[1][clockwiseRotator[i]])
 
-    rCube[1].forEach( (e, i) => rCube[1][i] = priorCube[1][counterClockwiseRotator[i]])
 }
